c301742a86bbd7f602dcf721cee1e4d7aa4a80c2
AD <103117507+trananhdat2710@users.noreply.github.com>
2025-05-25 12:11:15 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_a52sxq-user
-add_lunch_combo omni_a52sxq-userdebug
-add_lunch_combo omni_a52sxq-eng
+add_lunch_combo twrp_a52sxq-user
+add_lunch_combo twrp_a52sxq-userdebug
+add_lunch_combo twrp_a52sxq-eng
